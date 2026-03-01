0e4308206a2be43a85d17cd7b419b198909a14e1
tThanh <lt.thanh008@gmail.com>
2026-03-02 00:08:04 +0700

    Added comments and ceilings, removed uneccesaries

--- Template.cpp
+++ Template.cpp
@@ -3,13 +3,13 @@ using namespace std;
 
 #define NAME "main"
 #define MULTI_TEST 0 // 1 for multi-test cases, 0 for single test case
-#define FAST_IO ios::sync_with_stdio(0); cin.tie(nullptr);
+#define LTT ios::sync_with_stdio(0); cin.tie(nullptr);
 
 using lint = long long; //suits my workflow better than ll
+using pii = pair<int,int>;
 
 #define all(x) x.begin(), x.end()
 #define rall(x) x.rbegin(), x.rend()
-#define pb(x) push_back(x)
 
 template<class T1, class T2>
     bool maximize(T1 &a, T2 b){
@@ -36,7 +36,7 @@ void solve(){
 }
 
 int main() {
-    FAST_IO
+    LTT
     #ifdef LOCAL
         freopen(NAME".inp", "r", stdin);
         freopen(NAME".out", "w", stdout);
